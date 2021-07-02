21be87d7b96c92d5c0db6babb263d5c581088eab
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2021-07-02 23:18:25 +0530

    Add files via upload

---
+++ php/adminUpdate.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+header('location:adminSettings.php');
+
+$con=mysqli_connect('127.0.0.1:3307','root');
+if($con){
+    echo "connection done";
+}else{
+    echo "no connection";
+}
+
+mysqli_select_db($con,'userdb');
+
+
+
+$uname=$_POST['username'];
+$mail=$_POST['email'];
+$ag=$_POST['age'];
+$gen=$_POST['gender'];
+$ph=$_POST['phone'];
+$pr=$_POST['profession'];
+
+$p=" delete from profile where email = '$mail'; ";
+$result=mysqli_query($con,$p);
+$q="insert into profile values('$uname','$mail','$ag','$gen','$ph','$pr') ";
+$result=mysqli_query($con,$q);
+$num=mysqli_num_rows($result);
+
+if($num==1){
+    $_SESSION['username']=$uname;
+    $_SESSION['email']=$mail;
+    header('location:adminProfile.php');
+}else{
+    // echo '<script>alert("User not found")</script>';
+    header('location:invalid.php');
+}
+
+
+
+?>
